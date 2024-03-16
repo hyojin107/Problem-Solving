94fdf7a895b485b191602dcb8afee09f72b14cd5
Hyojin Song <49690185+hyojin107@users.noreply.github.com>
2024-03-17 00:43:26 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String read = br.readLine();
+        String[] numList = read.split(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.parseInt(numList[0]) + Integer.parseInt(numList[1]));
+        System.out.println(sb);
+    }
+}
